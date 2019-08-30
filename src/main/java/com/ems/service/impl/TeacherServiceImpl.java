56ab8e2d683ffa1968d33d0d61a4d619a41215f4
propersilmon <propersilmon@gmail.com>
2019-08-30 10:58:49 +0800

    propersilmon 8-30 10:56 添加了新的Realm，TeacherRealm和StudentRealm

--- src/main/java/com/ems/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/ems/service/impl/TeacherServiceImpl.java
@@ -1,8 +1,6 @@
 package com.ems.service.impl;
 
 import com.ems.service.TeacherService;
-import org.springframework.stereotype.Service;
 
-@Service
 public class TeacherServiceImpl implements TeacherService {
 }
