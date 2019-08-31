0cd25032a085cf0cfb472e53471aca75724531c8
Andre422436 <1013561957@qq.com>
2019-08-31 13:33:39 +0800

    lyh 8-31 13:33 添加RoomService(增删改查)

---
+++ src/main/java/com/ems/service/RoomService.java
@@ -0,0 +1,21 @@
+package com.ems.service;
+
+import com.ems.entity.Room;
+
+import java.util.List;
+
+public interface RoomService {
+    int deleteByPrimaryKey(Integer rId);
+
+    int insert(Room record);
+
+    int insertSelective(Room record);
+
+    Room selectByPrimaryKey(Integer rId);
+
+    int updateByPrimaryKeySelective(Room record);
+
+    int updateByPrimaryKey(Room record);
+
+    List<Room> queryRoomByPosition(String position);
+}
