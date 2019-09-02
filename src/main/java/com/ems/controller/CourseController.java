64c20f520d2ed59da93692f72137720d77322841
MrLiaoy <53252631+MrLiaoy@users.noreply.github.com>
2019-09-02 09:36:21 +0800

    廖益隆 2019,09,02,9.36

--- src/main/java/com/ems/controller/CourseController.java
+++ src/main/java/com/ems/controller/CourseController.java
@@ -2,13 +2,14 @@ package com.ems.controller;
 
 import com.ems.entity.Course;
 import com.ems.service.CourseService;
+import com.ems.vo.PageBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 /**
  * @program: EMS->CourseController
@@ -23,32 +24,66 @@ public class CourseController {
     CourseService courseService;
 
     @RequestMapping("/queryAll")
-    public String queryAll(){
-        List<Course> list=courseService.selectAll();
-        System.out.println(list);
+    public String queryAll(HttpServletRequest request, HttpServletResponse response, Model model) {
+        PageBean<Course> pageBean = new PageBean<>();
+        pageBean.setCurrentPageCode(Integer.parseInt(request.getParameter("page")));
+        //获取当前数据的最大页数
+        int maxPage = 0;
+        if (courseService.getCount() % 10 == 0) {
+            maxPage = courseService.getCount() / 10;
+        } else {
+            maxPage = courseService.getCount() / 10 + 1;
+        }
+
+        pageBean.setTotalPageCode(maxPage);
+        pageBean = courseService.selectAll(pageBean);
+        System.out.println(pageBean.getBeanList());
+        System.out.println(pageBean.getTotalPageCode());
+//        将数据传到model中
+        model.addAttribute("pageBean", pageBean);
+//        返回跳转页面的url
         return null;
 
     }
+
     @RequestMapping("/deletCourseById")
-    public String deletCourseById(HttpServletRequest request, HttpServletResponse response){
-        Course c=new Course();
+    public String deletCourseById(HttpServletRequest request, HttpServletResponse response) {
+        Course c = new Course();
         c.setcId(Integer.parseInt(request.getParameter("cid")));
         courseService.deletCourseById(c);
         return null;
     }
+
     @RequestMapping("/selectBycName")
-    public String selectBycName(HttpServletRequest request, HttpServletResponse response){
-        List<Course> list=courseService.selectBycName(request.getParameter("cName"));
-        System.out.println(list);
+    public String selectBycName(HttpServletRequest request, HttpServletResponse response) {
+        PageBean<Course> pagebean=new PageBean<Course>();
+        if (request.getParameter("cName") != null && !request.getParameter("cName").trim().equals("")) {
+            pagebean = courseService.selectBycName(request.getParameter("cName"),
+                    Integer.parseInt(request.getParameter("page")));
+        }else {
+            pagebean.setCurrentPageCode(Integer.parseInt(request.getParameter("page")));
+            //获取当前数据的最大页数
+            int maxPage = 0;
+            if (courseService.getCount() % 10 == 0) {
+                maxPage = courseService.getCount() / 10;
+            } else {
+                maxPage = courseService.getCount() / 10 + 1;
+            }
+
+            pagebean.setTotalPageCode(maxPage);
+            pagebean = courseService.selectAll(pagebean);
+        }
+        System.out.println(pagebean.getBeanList());
         return null;
     }
+
     @RequestMapping("/addCourse")
-    public String addCourse(HttpServletRequest request, HttpServletResponse response){
-        Course course=new Course();
+    public String addCourse(HttpServletRequest request, HttpServletResponse response) {
+        Course course = new Course();
         course.setcName(request.getParameter("cName"));
         course.seteId(Integer.valueOf(request.getParameter("eId")));
         course.setElective(Byte.valueOf(request.getParameter("Elective")));
-        course=courseService.addCourse(course);
+        course = courseService.addCourse(course);
 
         System.out.println(course);
         return null;
